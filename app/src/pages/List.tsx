e3236428f21010495a45fea37f94b7ba5aa1d9c7
Petrik Diak <diak@petrik.hu>
2024-02-26 13:14:27 +0100

    list

--- app/src/pages/List.tsx
+++ app/src/pages/List.tsx
@@ -1,5 +1,24 @@
 import Card from "../Lista/Card"
+import { useState } from "react";
+import data from "../data.json";
+import "./List.css";
 
 export default function List() {
-    return <Card src="https://www.cinemacity.hu/xmedia-cw/repo/feats/posters/5783D2R.jpg" name="Dűne - Második rész"/>
+    const [list, setList] = useState(data);
+
+    const search = (e : any) => {
+        const filtered = data.filter(i => i.name.toLowerCase().includes(e.target.value.toLowerCase()));
+        setList(filtered);
+    }
+
+    return (
+        <>
+        <div> 
+            <input type="text" id="searchbar" onChange={search}/>
+        </div>
+        <div className="container">
+            {list.map(e => <Card src={e.img} name={e.name} key={e.id}/>)}
+        </div>
+        </>
+    )
 }
