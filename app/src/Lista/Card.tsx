46e77213f749a83008c2c6d238e961bbd2ae4b42
RBence01 <rabel.bence15@gmail.com>
2024-02-26 19:07:25 +0100

    sob

--- app/src/Lista/Card.tsx
+++ app/src/Lista/Card.tsx
@@ -1,6 +1,10 @@
 import "./Card.css"
 
 export default function Card({src, name} : {src : string, name: string}) {
+    window.addEventListener("resize", (e) => {
+        document.documentElement.style.setProperty('--gridSize', Math.round(window.innerWidth / 250));
+        console.log(Math.round(window.innerWidth / 250));
+    });
     return (
         <div className="card">
             <img src={src}/>
