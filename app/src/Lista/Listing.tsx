eae460346c96dfceda83a3862026e1226edc0363
RBence01 <rabel.bence15@gmail.com>
2024-03-03 15:21:45 +0100

    listing scroll

--- app/src/Lista/Listing.tsx
+++ app/src/Lista/Listing.tsx
@@ -1,12 +1,24 @@
 import data from "../data.json";
 import Card from "./Card";
 import "./Listing.css";
+import leftArrow from "../public/left-arrow.png";
+import rightArrow from "../public/right-arrow.png";
 
 export default function Listing({limit} : {limit? : number}) {
     if (limit) data.slice(0, limit);
     return (
-        <div className="listing">
-            {data.map(e => <Card src={e.img} name={e.name} key={e.id}/>)}
-        </div> 
+        <div className="listing-container">
+            <button onClick={() => scroll(-350)}><img src={leftArrow}/></button>
+            <div className="listing">
+                {data.map(e => <Card src={e.img} name={e.name} key={e.id}/>)}
+            </div> 
+            <button onClick={() => scroll(350)}><img src={rightArrow}/></button>
+        </div>
     )
+}
+
+function scroll(n : number) {
+    let listing = document.getElementsByClassName("listing");
+    if (listing.length == 0) return;
+    listing[0].scrollBy({left: n, top: 0, behavior: "smooth"});
 }
