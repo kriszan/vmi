d18c96a3e3ff95bf796d6868af7daae411513701
Petrik Diak <diak@petrik.hu>
2024-03-04 12:52:57 +0100

    Mainpage text

--- app/src/MainPage.tsx
+++ app/src/MainPage.tsx
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
 import Lapozgatosizegenyo from "./Lapozgatosizegenyo"
+import Randomtext from "./Randomtxtfront"
 
 export default class MainPage extends React.Component {
     render() {
         return (<>
+        <Lapozgatosizegenyo/>
+            <Randomtext/>
             <Lapozgatosizegenyo/>
         </>
         );
