d89e1c92060e92f48d4a7687f900c1eb798989f1
Petrik Diak <diak@petrik.hu>
2024-03-04 13:09:58 +0100

    Featured added

--- app/src/MainPage.tsx
+++ app/src/MainPage.tsx
@@ -5,7 +5,6 @@ import Randomtext from "./Randomtxtfront"
 export default class MainPage extends React.Component {
     render() {
         return (<>
-        <Lapozgatosizegenyo/>
             <Randomtext/>
             <Lapozgatosizegenyo/>
         </>
--- app/src/Randomtxtfront.tsx
+++ app/src/Randomtxtfront.tsx
@@ -5,8 +5,9 @@ import img from "../src/public/mozi.png"
 export default class Randomtxt extends Component {
   render() {
     return (<>
+
+      <img src={img} className="imgmozi" />
       <div className="akarmi">
-        <img src={img} className="imgmozi"/>
         <h1 className="headline">Megnyitottunk!!!!</h1>
         <p className="text">Boldogan, örömmel ajánljuk figyelmükbe ujjonnal nyílt mozinkat. Legmodernebb film és hangtechnikai eszközökkel felszerelt mozink számos frissen megjelent blockbuster filmekkel várja Önöket a Nemtudommilyen utca 20. szám alatt. Online jegyvásárlási felületünk hamarosan elérhető lesz a kedves vásárlóink számára addig is jegyeket a Placeholder tér 92 szám alatt és a vetítés helyszínén tudnak venni. Ide még kell vmi szöveg placeholderként ugyhogy írok</p>
       </div>
